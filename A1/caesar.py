1f5304600fe5594a4db3ed5d4168ef209a7bb346
Bill Xiong <billxiong99@gmail.com>
2013-09-11 13:53:22 -0700

    First Working Version

--- A1/caesar.py
+++ A1/caesar.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python
 
-import sys, optparse
-
-
-
-
-
+import sys, argparse
 
 small = "abcdefghijklmnopqrstuvwxyz"
 big   = small.upper()
@@ -47,7 +42,26 @@ def caesar_bruteforce(S):
 	for i in range(0,26):
 		print caesar_decipher(S,i)
 
+parser = argparse.ArgumentParser(description='Encrypt or Decrypt using Caesar Cipher')
+parser.add_argument('text', help='The Plaintext or Ciphertext in question.')
+parser.add_argument('-e','--encode', action="store", dest="encode", type=int, help='The amount to shift by (if encoding)', required = False)
+parser.add_argument('-d','--decode', action="store", dest="decode", type=int, help='The Key when Decrypting', required = False)
 
+args = vars(parser.parse_args())
+if args['encode']:
+	print "Encoding %s with shift %d:" % (args['text'],args['encode'])
+	print caesar_encipher(args['text'],args['encode'])
+elif args['decode']:
+	print "Decoding %s with key %d:" % (args['text'],args['decode'])
+	print caesar_decipher(args['text'],args['decode'])
+else:
+	print "Bruteforcing %s:" % args['text']
+	print caesar_bruteforce(args['text'])
+
+
+
+
+'''
 # Encipher examples...
 print caesar_encipher("abcdefghijklmnopqrstuvwxyz", 1)
 print caesar_encipher("abcdefghijklmnopqrstuvwxyz", 2)
@@ -69,4 +83,4 @@ print "\n"
 # Both...
 string = "x0kf ku og!"
 print caesar_bruteforce(string)
-
+'''
