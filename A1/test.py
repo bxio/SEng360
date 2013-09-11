e8b4a9a23c7c53a803761e335cc80825f26e9888
Bill Xiong <billxiong99@gmail.com>
2013-09-11 13:55:09 -0700

    QuizHelp for Quiz 1 finished

--- A1/test.py
+++
@@ -1,8 +0,0 @@
-import optparse
-
-parser = optparse.OptionParser()
-
-parser.add_option('-b', help='boolean option', dest='bool', \
-    default=False, action='store_true')
-
-(opts, args) = parser.parse_args()
